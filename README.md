0af9ed238b113411b53673cd74f1d03e956a7540
Ahmed Nouira <ahmnouira@gmail.com>
2019-06-07 10:11:49 +0100

    micropython
    v1.0

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 #Getting Started With MicroPython on the ESP8266
+
+for more details visit this link:
+https://www.instructables.com/id/Getting-Started-With-MicroPython-on-the-ESP8266/
--- src/blink.py
+++ src/blink.py
@@ -1,10 +1,10 @@
 # blink LED every 1 second
-import machine 			# the machine module hols the pin configurations and modes.
-from time import sleep  			# import sleep for some delay
-led_pin = 5					# connect led to GPIO5 = 
-LED = machine.Pin(led_pin, machine.PIN.OUT)	# configure LED as OUTPUT
-while True:					# run forever
-    LED.value(1)				# set LED to HIGH
-    sleep(1)					# wait 1 second
-    LED.value(0)				# set LED to LOW
-    sleep(0)					# wait 1 second
+def blink(pin=5, time=1):		# blink function by default pin = 5, time = 1 you can change it
+    import machine 			# the machine module holds the pin configurations and modes.
+    from time import sleep  		# import sleep for some delay				
+    LED = machine.Pin(pin, machine.Pin.OUT)	# configure LED as OUTPUT
+    while True:			# run forever
+        LED.value(1)			# set LED to HIGH
+        sleep(time)			# wait 1 second by default
+        LED.value(0)			# set LED to LOW
+        sleep(time)			# wait 1 second by default
